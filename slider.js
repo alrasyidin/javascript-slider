a953af6ffed0eef71f1426259b105f1badd9806f
hamstergeek38 <hamstergeek38@gmail.com>
2017-10-19 11:20:22 +0700

    upload kode ke github

---
+++ slider.js
@@ -0,0 +1,8 @@
+function carousel(){
+    var div = document.querySelector('div#container div');
+    var jumlah = div.length;
+
+    // div.style.marginLeft = '-400px';
+}
+
+window.setInterval(carousel, 3000);
